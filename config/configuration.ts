84675dd1793b22e399922a652f7219c2a032342a
evantianx <evantianx@gmail.com>
2022-01-05 15:52:34 -0700

    chores: setup database and env files
    Signed-off-by: evantianx <evantianx@gmail.com>

---
+++ config/configuration.ts
@@ -0,0 +1,19 @@
+import { ConfigModuleOptions } from '@nestjs/config';
+import * as Joi from 'joi';
+
+export const configuration: ConfigModuleOptions = {
+  isGlobal: true,
+  envFilePath: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env',
+  ignoreEnvFile: process.env.NODE_ENV === 'prod',
+  validationSchema: Joi.object({
+    NODE_ENV: Joi.string().valid('dev', 'test', 'prod').default('dev'),
+    TYPEORM_CONNECTION: Joi.string().required(),
+    TYPEORM_HOST: Joi.string().required(),
+    TYPEORM_USERNAME: Joi.string().required(),
+    TYPEORM_PASSWORD: Joi.string().required(),
+    TYPEORM_DATABASE: Joi.string().required(),
+    TYPEORM_PORT: Joi.number().required(), // Joi is not able to validate port as number (it's string)
+    TYPEORM_SYNCHRONIZE: Joi.boolean().required(),
+    TYPEORM_LOGGING: Joi.boolean().required(),
+  }),
+} as const;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configuration } from '../config/configuration';
 
 @Module({
-  imports: [GraphQLModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(configuration),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
